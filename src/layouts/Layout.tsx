013e1c22fcc7cea50094d72819e0bbc7d1c15c1c
Andy Williams <andy@nonissue.org>
2022-02-05 17:27:36 -0700

    bump

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,10 +3,7 @@ type Props = {
 };
 
 function Layout({ children }: Props): React.ReactElement {
-  return (
-    // <div className=" font-sans text-slate-700 dark:text-slate-100 layout-bg">
-    <div className="padded-container">{children}</div>
-  );
+  return <div className="padded-container">{children}</div>;
 }
 
 export const getLayout = (page: React.ReactElement): React.ReactElement => (
